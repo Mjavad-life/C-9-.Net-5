5f73b0ecd1d8145c88dae7035a61d268159f5c65
Mjavad-life <Mjavadmosavi@gmail.com>
2024-05-27 14:47:58 +0330

    Add files via upload

---
+++ SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/SportCars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleCSharpConsoleApp
+{
+    public class SportCars : Car
+    {
+        public string GetPetName()
+        {
+            PetName = "fred";
+            return PetName;
+        }
+    }
+}
