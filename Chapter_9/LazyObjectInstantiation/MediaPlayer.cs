dd26e40e7058cebe669cc1f677a3ef13438f0d00
Mjavad-life <Mjavadmosavi@gmail.com>
2024-05-27 14:44:43 +0330

    Add files via upload

---
+++ Chapter_9/LazyObjectInstantiation/MediaPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyObjectInstantiation
+{
+    class MediaPlayer
+    {
+        // Assume these methods do something useful.
+        public void Play() { /* Play a song */ }
+        public void Pause() { /* Pause the song */ }
+        public void Stop() { /* Stop playback */ }
+
+        //private AllTracks _allSongs = new AllTracks();
+        // lazy bazi 
+        private Lazy<AllTracks> _allSongs = new Lazy<AllTracks>();
+        public AllTracks GetAllTracks()
+        {
+            // Return all of the songs.
+            //return _allSongs;
+
+            // update return for lazy correspondense
+            return _allSongs.Value;
+        }
+    }
+}
